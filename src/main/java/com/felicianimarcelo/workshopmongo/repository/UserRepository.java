c62efd8ef82f0e96c546c7053ddc6d66b259da92
Marcelo Feliciani <marcelo.feliciani@gmail.com>
2020-12-28 16:31:45 -0300

    Conectando ao MongoDB com Repository Service

---
+++ src/main/java/com/felicianimarcelo/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.felicianimarcelo.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.felicianimarcelo.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/felicianimarcelo/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.felicianimarcelo.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felicianimarcelo.workshopmongo.domain.User;
+import com.felicianimarcelo.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+
+}
